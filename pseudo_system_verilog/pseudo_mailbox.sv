998e6d09c044ab198eb7a8558540db7c644a1a31
DAEYEONG <nightmare2585@gmail.com>
2026-04-10 20:04:12 +0900

    [ADD] Descriptions for 'pseudo_mailbox.sv'

--- pseudo_system_verilog/pseudo_mailbox.sv
+++ pseudo_system_verilog/pseudo_mailbox.sv
@@ -1,3 +1,22 @@
+// =======================================================================
+// program Name : test
+// Author       : Kim Daeyeong
+// Date         : 2026-04-10
+//
+// Description	:
+// 	This code implements a producer-consumer model using SystemVerilog.
+// 	A queue is used as a FIFO buffer, where the producer inserts data
+// 	using push_back and the consumer retrieves data using pop_front.
+// 	Synchronization between the producer and consumer is achieved using
+// 	events, forming a simple handshake mechanism to prevent race
+// 	conditions and ensure proper data transfer order.
+//
+// Notes	:
+// 	-Additional event slot_ready was added cause not using mailbox.
+// 	-Queue can do back and front for both push and pop.
+//
+// =======================================================================
+
 program test;
 	event data_ready, slot_ready;
 	class Producer;
